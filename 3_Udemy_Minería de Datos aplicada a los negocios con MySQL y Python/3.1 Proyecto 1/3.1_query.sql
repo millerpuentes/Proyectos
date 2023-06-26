5ec81cf9f2dadedd0fc828a567e97c8a6a20d4f1
Miller Puentes <miller.puentes@gmail.com>
2023-06-26 12:03:37 -0500

    Proyecto1

--- 3_Udemy_Minería de Datos aplicada a los negocios con MySQL y Python/3.1 Proyecto 1/3.1_query.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    -- CONCAT(YEAR(t1.OrderDate), "-", MONTH(t1.OrderDate)) AS perido,
-    COUNT(DISTINCT t1.SalesOrderID) as transacciones,
-    ROUND(SUM(t1.TotalDue),2) AS ventas
-FROM nuevo_esquema.salesorderheader AS t1
-	GROUP BY YEAR(t1.OrderDate),MONTH(t1.OrderDate)
-    ORDER BY YEAR(t1.OrderDate),MONTH(t1.OrderDate);
